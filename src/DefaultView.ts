d79fa767340d0f9bf58965eb5d6bf87bfb9076e2
anticultist <33195947+anticultist@users.noreply.github.com>
2025-08-12 22:12:05 +0200

    partially added a webview

---
+++ src/DefaultView.ts
@@ -0,0 +1,46 @@
+import * as vscode from "vscode";
+
+export class DefaultView implements vscode.WebviewViewProvider {
+  public static readonly viewType = "timeTraceLocalDefaultView";
+  private static instance: DefaultView;
+
+  private view?: vscode.WebviewView;
+
+  constructor(private readonly extensionUri: vscode.Uri) {}
+
+  public static getInstance(extensionUri: vscode.Uri): DefaultView {
+    if (!DefaultView.instance) {
+      DefaultView.instance = new DefaultView(extensionUri);
+    }
+    return DefaultView.instance;
+  }
+
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    _context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken
+  ): Thenable<void> | void {
+    this.view = webviewView;
+
+    this.view.webview.options = {
+      enableScripts: true,
+      localResourceRoots: [this.extensionUri],
+    };
+
+    this.view.webview.html = this.getHtml(this.view.webview);
+  }
+
+  private getHtml(webview: vscode.Webview) {
+    return `<!DOCTYPE html>
+    <html lang="end">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial=scale=1.0" >/
+        <title>Time Trace Local</title>
+      </head>
+      <body>
+        Hello World!
+      </body>
+    </html>`;
+  }
+}
