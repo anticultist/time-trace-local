3b85920b296d9a13859606f0b05572aa2fe88469
anticultist <33195947+anticultist@users.noreply.github.com>
2025-09-29 21:50:10 +0200

    add dummy configuration

--- packages/extension/src/extension.ts
+++ packages/extension/src/extension.ts
@@ -3,6 +3,50 @@
 import * as vscode from "vscode";
 import { DefaultView } from "./DefaultView";
 
+async function showConfigurationQuickPick() {
+  const options: vscode.QuickPickItem[] = [
+    {
+      label: "boot",
+      description: "Boot event tracking",
+    },
+    {
+      label: "login",
+      description: "Login event tracking",
+    },
+    {
+      label: "sleep",
+      description: "Sleep event tracking",
+    },
+  ];
+
+  const quickPick = vscode.window.createQuickPick();
+  quickPick.items = options;
+  quickPick.canSelectMany = true;
+  quickPick.title = "Configure Time Trace Local";
+  quickPick.placeholder = "Select the events you want to track";
+
+  quickPick.onDidAccept(() => {
+    const selectedItems = quickPick.selectedItems;
+    const selectedLabels = selectedItems.map((item) => item.label);
+
+    if (selectedLabels.length > 0) {
+      vscode.window.showInformationMessage(
+        `Selected configuration: ${selectedLabels.join(", ")}`
+      );
+    } else {
+      vscode.window.showInformationMessage("No configuration selected");
+    }
+
+    quickPick.dispose();
+  });
+
+  quickPick.onDidHide(() => {
+    quickPick.dispose();
+  });
+
+  quickPick.show();
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -28,8 +72,22 @@ export function activate(context: vscode.ExtensionContext) {
     }
   );
 
-  context.subscriptions.push(defaultViewProvider, refreshCommand);
+  // Register the configure command
+  const configureCommand = vscode.commands.registerCommand(
+    "timeTraceLocal.configure",
+    async () => {
+      await showConfigurationQuickPick();
+    }
+  );
+
+  context.subscriptions.push(
+    defaultViewProvider,
+    refreshCommand,
+    configureCommand
+  );
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+  // Currently no cleanup needed
+}
