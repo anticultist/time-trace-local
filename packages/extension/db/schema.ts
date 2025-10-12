0a955322344262558e3bd2d70d03e49c4a59c309
anticultist <33195947+anticultist@users.noreply.github.com>
2025-10-12 21:53:37 +0200

    add db schema

---
+++ packages/extension/db/schema.ts
@@ -0,0 +1,34 @@
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export type PropertyValue = string | number;
+
+export enum PropertyType {
+  Text = 0,
+  Integer = 1,
+  Real = 2,
+}
+
+export const dbProperties = sqliteTable("db_properties", {
+  id: int().primaryKey({ autoIncrement: true }),
+  name: text().notNull().unique(),
+  type: int("type").notNull().$type<PropertyType>(),
+  value: text("value", { mode: "json" }).$type<PropertyValue>().notNull(),
+});
+
+export type DbProperty = typeof dbProperties.$inferSelect;
+export type NewDbProperty = typeof dbProperties.$inferInsert;
+
+export type EventType =
+  | "boot"
+  | "shutdown"
+  | "logon"
+  | "logoff"
+  | "standby_enter"
+  | "standby_exit";
+
+export const events = sqliteTable("events", {
+  id: int().primaryKey({ autoIncrement: true }),
+  time: int().notNull(),
+  type: text().notNull().$type<EventType>(),
+  details: text().notNull(),
+});
